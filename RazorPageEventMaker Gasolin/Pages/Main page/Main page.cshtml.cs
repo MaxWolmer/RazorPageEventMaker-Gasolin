57d852386f2eed779511b19a07d196fdca4ace9d
Elvie-Abarth <estibrusse18@gmail.com>
2025-03-18 16:15:02 +0100

    Add wab page folders

---
+++ RazorPageEventMaker Gasolin/Pages/Main page/Main page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageEventMaker_Gasolin.Pages.Main_page
+{
+    public class Main_pageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RazorPageEventMaker Gasolin/Pages/Timeline page/Timelinepage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageEventMaker_Gasolin.Pages.Timeline_page
+{
+    public class TimelinepageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
