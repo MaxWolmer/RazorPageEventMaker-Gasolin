362659e41fd35239f538a0421e22df3dba70d1a9
Elvie-Abarth <estibrusse18@gmail.com>
2025-03-19 19:59:49 +0100

    all changes

---
+++ RazorPageEventMaker Gasolin/Pages/BioIntervieuw page/Biografier.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageEventMaker_Gasolin.Pages.BioIntervieuw  // Skal matche den præcise mappenavn!
+{
+    public class BiografierModel : PageModel  // Klassenavn skal matche @model i .cshtml
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RazorPageEventMaker Gasolin/Pages/Tidslinje.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageEventMaker_Gasolin.Pages;
+
+public class Tidslinje : PageModel
+{
+    private readonly ILogger<Tidslinje> _logger;
+
+    public Tidslinje(ILogger<Tidslinje> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
