ddf50e392d2ac11d76e82b08476ad7b57aaf782d
Martin Wolmer Tran Jensen <maj022@edu.zealand.dk>
2025-03-19 13:04:34 +0100

    I accidentally tried to help with the Biography pages. There are issues with namespace i believe.

--- RazorPageEventMaker Gasolin/Pages/BioIntervieuw page/BioInervieuwpage.cshtml.cs
+++ RazorPageEventMaker Gasolin/Pages/BioIntervieuw page/BioInervieuwpage.cshtml.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorPageEventMaker_Gasolin.Pages.BioIntervieuw_page
 {
     public class BioInervieuwpageModel : PageModel
     {
