2398d8ee01323bd07c7bc56c6e545c2a67006c8f
MaxWolmer <mwtj1000@gmail.com>
2025-03-20 10:41:26 +0100

    deleted Timeline file

---
+++ RazorPageEventMaker Gasolin/Pages/BioIntervieuw page/Biografier.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageEventMaker_Gasolin.Pages.BioInterview
+
+{
+    public class Biografier : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
